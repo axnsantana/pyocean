f431630451cc8daa76e5a8c71c5d522785e65b2e
Alex Santana <alexsantana@santana-2.local>
2016-08-30 01:26:28 +0200

    Including new function for SOCIB source, adjusting the config file. Including parameters args e kargs in getdata function

--- getdata/getdata.py
+++ getdata/getdata.py
@@ -1,10 +1,10 @@
 import sys
 
-def getdata(module_name,function_name,args=None):
+def getdata(module_name,function_name,*args,**kargs):
     import importlib
     try:
         module = importlib.import_module(module_name)
-        return getattr(module,function_name)(args)
+        return getattr(module,function_name)(*args,**kargs)
     except Exception, e:
         print( "<p>Error: %s</p>" % e )
         
--- getdata/modules/socib/socib.py
+++ getdata/modules/socib/socib.py
@@ -4,16 +4,38 @@ basepath = os.path.dirname(__file__)
 cfg_file=os.path.abspath(os.path.join(basepath,"socib.yml"))
 config = yaml.safe_load(open(cfg_file))
 
-def get_waves_forecast_data(varname):
+def get_waves_forecast_data(varname,str_date):
     from pydap.client import open_url
     from datetime import datetime as dt
     
-    date = dt.strptime('20160828000000','%Y%m%d%H%M%S')
+    print varname
+    print str_date
+    date = dt.strptime(str_date,'%Y%m%d%H%M%S')
     waves_cfg = config['products']['modeling']['wave']
     url_base = waves_cfg['urls']['opendap']
-    ncfile = dt.strftime(date,'%Y/%m/sapo_ib_swan_%Y%m%d%H%M%S.nc')
-    url = '%s/%s' % (url_base,ncfile) 
+    ncfile = waves_cfg['basename'] % str_date
+    prefix = dt.strftime(date,'%Y/%m')
+    url = '%s/%s/%s' % (url_base,prefix,ncfile)
+    print url
     dataset = open_url(url)
     
     return dataset[varname]
 
+def get_fixed_station_data(station,varname,str_date=None):
+    from pydap.client import open_url
+    from datetime import datetime as dt
+    
+    date = dt.strptime(str_date,'%Y%m%d%H%M%S')
+    station_cfg = config['products']['observational']['fixed_estations'][station]
+    ncfile = station_cfg['basename'] % 'latest'
+    prefix = ''
+    if(str_date is not None):
+        ncfile = station_cfg['basename'] % dt.strftime(date,'%Y-%m')
+        prefix = dt.strftime(date,'%Y')
+    url_base = station_cfg['urls']['opendap']
+    url = '%s/%s/%s' % (url_base,prefix,ncfile)
+    print url
+    dataset = open_url(url)
+    
+    return dataset[varname]
+    
